dcadf0a8170805c2e5ab0d76da2d3c8220af980c
lzh594 <liuzh594@qq.com>
2024-03-26 14:14:16 +0800

    update bubble

--- sort/src/bubble.rs
+++ sort/src/bubble.rs
@@ -1,3 +1,19 @@
+/// # 冒泡排序
+///
+/// # Arguments
+///
+/// * `array`: 待排序数组
+///
+/// returns: ()
+///
+/// # Examples
+///
+/// ```
+/// use sort::bubble::bubble_sort;
+/// let mut nums = vec![21, 43, 555, 12, 43, 432, 8765, 132, 6543, 11, 0, 24321];
+/// bubble_sort(&mut nums);
+/// assert_eq!(nums, vec![0, 11, 12, 21, 43, 43, 132, 432, 555, 6543, 8765, 24321]);
+/// ```
 pub fn bubble_sort<T: PartialOrd>(array: &mut [T]) {
     let len = array.len();
     if len <= 1 {
@@ -6,12 +22,12 @@ pub fn bubble_sort<T: PartialOrd>(array: &mut [T]) {
     for i in 0..len as usize {
         let mut flag = false;
         for j in 0..(len - i - 1) as usize {
-            if array[j] > array[j - 1] {
-                array.swap(j - 1, j);
+            if array[j] > array[j + 1] {
+                array.swap(j, j+1);
                 flag = true;
             }
         }
-        if flag {
+        if !flag {
             break;
         }
     }
