7cd2d4fc933beb19f879d4f5e2a95611e339e848
lzh594 <liuzh594@qq.com>
2024-04-02 14:40:40 +0800

    update data_structures/lists

--- src/data_structures/lists/fourth.rs
+++ src/data_structures/lists/fourth.rs
@@ -1,4 +1,5 @@
-use std::cell::{Ref, RefCell};
+//! # 不完整的双向链表
+use std::cell::{Ref, RefCell, RefMut};
 use std::rc::Rc;
 
 pub struct List<T> {
@@ -14,6 +15,8 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+pub struct Iter<'a, T>(Option<Ref<'a, Node<T>>>);
 impl<T> Node<T> {
     pub fn new(elem: T) -> Rc<RefCell<Self <>>> {
         Rc::new(RefCell::new(Node {
@@ -46,6 +49,23 @@ impl<T> List<T> {
             }
         }
     }
+    /// 向 List 尾部推入一个元素
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                // 非空链表，将新的节点跟老的尾部相连接
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                // 空链表，需要设置 tail 和 head
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
     /// 从 List 头部弹出一个元素 
     pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|old_head| {
@@ -63,11 +83,56 @@ impl<T> List<T> {
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
         })
     }
+    /// 从 List 尾部弹出一个元素
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().next.take() {
+                Some(new_tail) => {
+                    // 非空
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    // 空链表
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
     /// 获取 List 头部节点的元素引用
     pub fn peek_front(&self) -> Option<Ref<T>> {
         self.head.as_ref().map(|node| {
             Ref::map(node.borrow(), |node| &node.elem)
         })
+    }
+    /// 获取 List 尾部节点的元素引用
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+    /// 获取 List 头部节点的元素可变引用
+    pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
+        self.head.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+    /// 获取 List 尾部节点的元素可变引用
+    pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
+        self.tail.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+    /// 迭代器，获取所有权
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+    /// 迭代器，不可变引用
+    pub fn iter(&self)->Iter<T>{
+        Iter(self.head.as_ref().map(|head| {
+            head.borrow()
+        }))
     }
 }
 
@@ -76,6 +141,29 @@ impl<T> Drop for List<T> {
         // 将每个节点 pop 出去，直到获得 None
         while self.pop_front().is_some() {}
     }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.0.pop_front()
+    }
+}
+// impl<'a, T> Iterator for Iter<'a, T> {
+//     type Item = Ref<'a, T>;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         self.0.take().map(|node_ref| {
+//             self.0 = node_ref.next.as_ref().map(|head| head.borrow());
+//             Ref::map(node_ref, |node| &node.elem)
+//         })
+//     }
+// }
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.0.pop_back()
+    }
+
 }
 
 #[cfg(test)]
@@ -109,14 +197,62 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // ---- back -----
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
+
     #[test]
     fn peek() {
         let mut list = List::new();
         assert!(list.peek_front().is_none());
-        list.push_front(1); list.push_front(2); list.push_front(3);
-        // Ref 不能被直接比较，因此我们需要先利用 Deref 解引用出其中的值，再进行比较。
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_front_mut().is_none());
+        assert!(list.peek_back_mut().is_none());
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
         assert_eq!(&*list.peek_front().unwrap(), &3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
+    }
+    #[test]
+    fn text_into_iter() {
+        let mut list = List::new();
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
     }
 
 }
--- src/data_structures/lists/mod.rs
+++ src/data_structures/lists/mod.rs
@@ -2,3 +2,5 @@ pub mod first;
 pub mod second;
 pub mod third;
 pub mod fourth;
+pub mod fifth;
+mod double_linked_deque;
