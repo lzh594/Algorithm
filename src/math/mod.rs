28e193312b72c3e94eac13afb77d2adc5700a543
lzh594 <liuzh594@qq.com>
2024-04-11 17:19:18 +0800

    update math

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,11 @@
 pub mod combinations;
 pub mod gcd;
 pub mod number_theory;
+pub mod matrix;
 
 pub use number_theory::fast_power::fast_power;
+pub use number_theory::egcd::egcd;
+pub use number_theory::invmod::invmod;
+pub use number_theory::chinese_remainder_theorem::crt;
 pub use combinations::combinations;
 pub use gcd::gcd;
