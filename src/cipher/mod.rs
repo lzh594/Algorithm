196120464d93967a01b3ed2c4ee73ce196149500
lzh594 <liuzh594@qq.com>
2024-04-21 17:16:25 +0800

    update cipher

--- src/cipher/mod.rs
+++ src/cipher/mod.rs
@@ -1,5 +1,6 @@
 pub mod caesar;
 pub mod vigenere;
+pub mod base64;
 
 pub use caesar::{
     caesar_encode,
@@ -9,4 +10,8 @@ pub use caesar::{
 pub use vigenere::{
     vigenere_encode,
     vigenere_decode,
+};
+pub use base64::{
+    base64_encode,
+    base64_decode,
 };
